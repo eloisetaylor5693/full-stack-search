4cc02e1d57fa5235ee6c3fce5b1934e283d7bbc4
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2025-02-09 16:45:31 +0000

    Extend hotels api to accept a search term
    Named the search term `freetext-search` to make it as clear as possible that the value is used for search, and that
    it's a free-text search rather than a facet filtering.
    
    I think the name gives room for other types of search, eg a location search where it's only searching by location.

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -19,6 +19,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/hotels', async (req, res) => {
+  const freetextSearchValue = req?.query?.['freetext-search'] as string;
+
   const mongoClient = new MongoClient(DATABASE_URL);
   console.log('Connecting to MongoDB...');
 
@@ -26,8 +28,18 @@ app.get('/hotels', async (req, res) => {
     await mongoClient.connect();
     console.log('Successfully connected to MongoDB!');
     const db = mongoClient.db();
-    const collection = db.collection('hotels');
-    res.send(await collection.find().toArray());
+
+    const query = {
+      $or: [
+        { chain_name: { $regex: freetextSearchValue, $options: 'i' } },
+        { hotel_name: { $regex: freetextSearchValue, $options: 'i' } },
+        { country: { $regex: freetextSearchValue, $options: 'i' } },
+        { city: { $regex: freetextSearchValue, $options: 'i' } },
+      ],
+    };
+
+    const collection = db.collection('hotels').find(query);
+    res.send(await collection.toArray());
   } finally {
     await mongoClient.close();
   }
