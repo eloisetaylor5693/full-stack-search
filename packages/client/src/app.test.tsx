baa7b270e821d932f86684534f87217412ce1b59
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2025-02-09 17:26:27 +0000

    Extend unit test

--- packages/client/src/app.test.tsx
+++ packages/client/src/app.test.tsx
@@ -1,5 +1,5 @@
-import { expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import App from './app';
 
@@ -7,4 +7,25 @@ test('renders search input', () => {
   render(<App />);
   const input = screen.getByPlaceholderText('Search accommodation...');
   expect(input).toBeInTheDocument();
+});
+
+describe('When user enters a search term', () => {
+  const searchTerm = 'marriot';
+
+  test('renders text input by user in the input box', () => {
+    render(<App />);
+    let input: HTMLInputElement = screen.getByPlaceholderText(
+      'Search accommodation...',
+    );
+
+    fireEvent.change(input, { target: { value: searchTerm } });
+
+    input = screen.getByPlaceholderText('Search accommodation...');
+    screen.debug();
+
+    expect(input.value).toBe(searchTerm);
+
+    const searchTearmInSearchBox = screen.getByDisplayValue(searchTerm);
+    expect(searchTearmInSearchBox).toBeInTheDocument();
+  });
 });
